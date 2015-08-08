7faf426b11f7adefbf7da4e496428ee6e7847075
Thomas Waldmann <tw@waldmann-edv.de>
2015-08-08 20:02:23 +0200

    try codecov as root on osx

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -7,5 +7,10 @@ NO_COVERAGE_TOXENVS=(pep8)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
     ln .tox/.coverage .coverage
-    codecov -e TRAVIS_OS_NAME TOXENV
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        # tests run as root, so access coverage file also as root
+        sudo codecov -e TRAVIS_OS_NAME TOXENV
+    else
+        codecov -e TRAVIS_OS_NAME TOXENV
+    fi
 fi
