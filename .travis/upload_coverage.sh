6599021cec9aa90487e341b8e0bf2ea0e78e99aa
Thomas Waldmann <tw@waldmann-edv.de>
2015-08-08 20:28:22 +0200

    make .coverage accessable on OS X

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -7,10 +7,7 @@ NO_COVERAGE_TOXENVS=(pep8)
 if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
     ln .tox/.coverage .coverage
-    if [[ "$(uname -s)" == "Darwin" ]]; then
-        # tests run as root, so access coverage file also as root
-        sudo codecov -e TRAVIS_OS_NAME TOXENV
-    else
-        codecov -e TRAVIS_OS_NAME TOXENV
-    fi
+    # on osx, tests run as root, need access to .coverage
+    sudo chmod 666 .coverage
+    codecov -e TRAVIS_OS_NAME TOXENV
 fi
